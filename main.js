314fec87afbd0f31fb23abcbfa07c8200d891dd7
Thomas Koshy <ttkoshy@gmail.com>
2023-04-13 19:01:50 -0400

    check the tech

---
+++ main.js
@@ -0,0 +1,56 @@
+//Array squared
+
+//const input = [1, 2, 3, 4, 5];
+//result [1, 4, 9, 16, 25];
+
+function square(arr) {
+    const squaredArr = arr.map(n => Math.pow(n, 2));
+    return squaredArr
+  }
+  
+  //console.log(square(input))
+  
+  //Sum of every positive element
+  
+  //const input = [1, -4, 12, 0, -3, 29, -150];
+  
+  function positiveSum(arr) {
+    const filterPositives = arr.filter(n => n > 0);
+    const sumPositives = filterPositives.reduce((acc, cur) => acc + cur, 0)
+    return sumPositives 
+  }
+  
+  //console.log(positiveSum(input))
+  
+  //Calculate Median and Mean
+  
+  //const input = [12, 46, 32, 64];
+  //result  { mean: 38.5, median: 39 }
+  
+  function medianAndMean(arr) {
+    const mean = arr.reduce((acc, cur) => acc+cur, 0)/arr.length
+    console.log(mean)
+    const resultObj = { mean: 0, median: 0 }
+    resultObj["mean"] = mean
+    const sortArr = arr.sort()
+    console.log(sortArr)
+    const median = (sortArr[1] + sortArr[2])/2
+    resultObj["median"] = median
+    return resultObj;
+  }
+  
+  //console.log(medianAndMean(input))
+  
+  //Get name initials
+  
+  //const input = "George Raymond Richard Martin";
+  
+  function initialize(str) {
+    const strSplit = str.split(' ')
+    const initial = strSplit.map(name => name[0])
+    const joinInitial = initial.join('')
+    return joinInitial
+  }
+  
+  //console.log(initialize(input))
+  
